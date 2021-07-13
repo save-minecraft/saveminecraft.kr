05165f75ea1033f2592e65e53c77c7968d86dd49
alex4386 <psh010209@gmail.com>
2021-07-14 02:48:32 +0900

    chore: add up-to-date news

--- pages/press/index.vue
+++ pages/press/index.vue
@@ -8,6 +8,25 @@
 
     article.p-4.max-w-6xl.m-auto
       h1.lv1.text-2xl.mb-2
+        | saveminecraft.kr 소식
+      ul.grid.grid-cols-1.gap-4(class='lg:grid-cols-2')
+        li(v-for='news in press')
+          a.group.block.rounded-lg.p-4.bg-gray-200.transition(:href='news.url' class='hover:bg-green-500 hover:border-transparent hover:shadow-lg')
+            dl.items-center
+              .flex.justify-between.flex-col(class="lg:flex-row lg:items-center")
+                div
+                  p.leading-6.font-medium(class='group-hover:text-white')
+                    | {{ news.title }}
+                  p.text-sm.font-medium.text-gray-500(class='group-hover:text-green-200' v-if="news.publisher")
+                    | {{ news.publisher }}
+                  p.text-sm.font-medium.text-gray-500(class='group-hover:text-green-200')
+                    | {{ $dayjs(news.releasedAt).format('YYYY년 MM월 DD일') }}
+
+      .mt-8
+      hr.hr.border-gray-500
+      .mt-8
+
+      h1.lv1.text-2xl.mb-2
         | 최신 뉴스
       ul.grid.grid-cols-1.gap-4(class='lg:grid-cols-2')
         li(v-for='news in newses')
@@ -31,17 +50,50 @@ export default Vue.extend({
   layout: 'default',
   data () {
     return {
+      press: [
+        {
+          title: '미성년자 이용 불가 사태, <마인크래프트>로 온라인 작품 전시회 개최한다',
+          releasedAt: '2021-07-12',
+          publisher: '우리 마인크래프트 공간',
+          url: 'https://cafe.naver.com/minecraftgame/1819898'
+        },
+        {
+          title: '[보도자료] 대표적 교육용 게임 <마인크래프트>, 올해부턴 성인용… ‘만 18세 미만’ 이용 제한에 발목',
+          releasedAt: '2021-07-02',
+          publisher: '우리 마인크래프트 공간',
+          url: 'https://cafe.naver.com/minecraftgame/1818027'
+        }
+      ],
       newses: [
+        {
+          title: '마인크래프트 속 셧다운 폐지 청원 전시회 열린다',
+          publisher: '전자신문',
+          releasedAt: '2021-07-12',
+          url: 'https://www.etnews.com/20210712000176'
+        },
+        {
+          title: "미성년자 이용 불가 사태, '마인크래프트'로 온라인 작품 전시회 개최한다",
+          publisher: '디스이즈게임',
+          releasedAt: '2021-07-12',
+          url: 'https://www.thisisgame.com/webzine/game/nboard/225/?n=128426'
+        },
+        {
+          title: "'마인크래프트' 유저 모임 '우마공', '마크' 활용한 셧다운제 폐지 청원 전시회 16일 온라인 개최",
+          publisher: '게임포커스',
+          releasedAt: '2021-07-12',
+          url: 'http://gamefocus.co.kr/detail.php?number=119420'
+        },
+        {
+          title: '셧다운제 폐지 청원, 마인크래프트로 한다...전시회 개최',
+          publisher: '테크M',
+          releasedAt: '2021-07-12',
+          url: 'https://www.techm.kr/news/articleView.html?idxno=85890'
+        },
         {
           title: "'마인크래프트'로 만드는 셧다운제 폐지 청원 전시회 열려",
           publisher: '아이뉴스24',
           releasedAt: '2021-07-12',
-          url: 'https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=105&oid=031&aid=0000610005'
-        },
-        {
-          title: '미성년자 이용 불가 사태, <마인크래프트>로 온라인 작품 전시회 개최한다',
-          releasedAt: '2021-07-12',
-          url: 'https://cafe.naver.com/minecraftgame/1819898'
+          url: 'http://www.inews24.com/view/1384421'
         }
       ]
     }
